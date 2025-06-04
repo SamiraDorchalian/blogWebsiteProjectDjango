0a8f53f1cae9d6ef9c348474487cad972f4ece10
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-04 15:49:55 +0330

    shoe posts_list in site

--- blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
+++ blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def post_list_view(reuest):
+    posts_list = Post.objects.all()
+    return render(reuest, 'blog/posts_list.html', {'posts_list': posts_list})
+
