bb7fa844cb635ee34bcf597a67bb236f22216eaf
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-04 17:23:59 +0330

    Getting input via URL to View

--- blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
+++ blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Post
 
@@ -6,3 +7,6 @@ def post_list_view(reuest):
     posts_list = Post.objects.all()
     return render(reuest, 'blog/posts_list.html', {'posts_list': posts_list})
 
+def post_detail_view(request, pk):
+    print('ID IN URL:', pk)
+    return HttpResponse(f'Id: {pk}')
