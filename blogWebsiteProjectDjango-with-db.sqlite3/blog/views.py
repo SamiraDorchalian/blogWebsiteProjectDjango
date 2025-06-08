761eb497352f4e67892be981fd152a60d3d5acbd
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-08 11:52:16 +0330

    make button update and order list

--- blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
+++ blogWebsiteProjectDjango-with-db.sqlite3/blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 from .forms import NewPostForm
 
 def post_list_view(reuest):
-    posts_list = Post.objects.filter(status='pub')
+    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
     return render(reuest, 'blog/posts_list.html', {'posts_list': posts_list})
 
 def post_detail_view(request, pk):
@@ -30,6 +30,7 @@ def post_update_view(request, pk):
 
     if form.is_valid():
         form.save()
+        return redirect('posts_list')
 
     return render(request, 'blog/post_create.html', context={'form': form})
 
