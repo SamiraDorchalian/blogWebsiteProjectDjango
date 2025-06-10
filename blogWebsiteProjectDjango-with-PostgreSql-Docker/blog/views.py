e570cd8193f001f63679995c3952d41a356a9265
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-10 19:23:38 +0330

    make listView by class-based

--- blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/views.py
+++ blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect, reverse
 from django.shortcuts import get_object_or_404
+from django.views import generic
 
 from .models import Post
 from .forms import NewPostForm
 
-def post_list_view(request):
-    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
-    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+
+class PostListView(generic.ListView):
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-datetime_modified')
+
 
 def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
