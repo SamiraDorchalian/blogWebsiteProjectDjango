12601210c7fca3bb271643be4f363355e2d2cea8
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-10 19:46:27 +0330

    make Detail&Create Views by class-based

--- blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/models.py
+++ blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/models.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse
 from django.db import models
 
 class Post(models.Model):
@@ -15,3 +16,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[self.id])
+    
--- blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/views.py
+++ blogWebsiteProjectDjango-with-PostgreSql-Docker/blog/views.py
@@ -5,7 +5,6 @@ from django.views import generic
 from .models import Post
 from .forms import NewPostForm
 
-
 class PostListView(generic.ListView):
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts_list'
@@ -14,19 +13,15 @@ class PostListView(generic.ListView):
         return Post.objects.filter(status='pub').order_by('-datetime_modified')
 
 
-def post_detail_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
-def post_create_view(request):
-    if request.method == 'POST':
-        form = NewPostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('posts_list')
-    else:
-        form = NewPostForm()
-    return render(request, 'blog/post_create.html', context={'form': form})
+
+class PostCreateView(generic.CreateView):
+    form_class = NewPostForm
+    template_name = 'blog/post_create.html'
 
 def post_update_view(request, pk):
     post = get_object_or_404(Post, pk=pk)  
