250ae10f3987b40f63c594353c672b2b93fc745c
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-11 14:54:01 +0330

    install and add crispy_forms

--- blogWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
+++ blogWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'crispy_forms',
+    'crispy_bootstrap4',
+
     'blog',
 ]
 
@@ -131,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
