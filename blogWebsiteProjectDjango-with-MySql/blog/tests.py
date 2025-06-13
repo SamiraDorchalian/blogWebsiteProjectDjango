f909a4492a4d212a3aeee7c2949accadccfd79d0
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-13 19:58:48 +0330

    add some test

--- blogWebsiteProjectDjango-with-MySql/blog/tests.py
+++ blogWebsiteProjectDjango-with-MySql/blog/tests.py
@@ -9,17 +9,25 @@ class BlogPostTest(TestCase):
     def setUpTestData(cls):
         cls.user = User.objects.create(username='user1')
         cls.post1 = Post.objects.create(
-            title = 'Post1',
-            text = 'This is the description of Post1',
-            status = Post.STATUS_CHOICES[0][0],
-            author = cls.user,
+            title='Post1',
+            text='This is the description of Post1',
+            status=Post.STATUS_CHOICES[0][0],
+            author=cls.user,
         )
         cls.post2 = Post.objects.create(
-            title = 'Post2',
-            text = 'This is the description of Post2',
-            status = Post.STATUS_CHOICES[1][0],
-            author = cls.user,
+            title='Post2',
+            text='This is the description of Post2',
+            status=Post.STATUS_CHOICES[1][0],
+            author=cls.user,
         )
+
+    def test_post_model_str(self):
+        post = self.post1
+        self.assertEqual(str(post), post.title)
+
+    def test_post_detail(self):
+        self.assertEqual(self.post1.title, 'Post1')
+        self.assertEqual(self.post1.text, 'This is the description of Post1')
 
     def test_post_list_url(self):
         response = self.client.get('/blog/')
@@ -32,7 +40,7 @@ class BlogPostTest(TestCase):
     def test_post_title_on_blog_list_page(self):
         response = self.client.get(reverse('posts_list'))
         self.assertContains(response, self.post1.title)
-
+    
     def test_post_detail_url(self):
         response = self.client.get(f'/blog/{self.post1.id}/')
         self.assertEqual(response.status_code, 200)
@@ -46,11 +54,37 @@ class BlogPostTest(TestCase):
         self.assertContains(response, self.post1.title)
         self.assertContains(response, self.post1.text)
 
-    def test_status_404_if_post_id_not_exist(self):
+    def test_status_404_if_post_is_not_exist(self):
         response = self.client.get(reverse('post_detail', args=[999]))
         self.assertEqual(response.status_code, 404)
 
     def test_draft_post_not_show_in_posts_list(self):
-        response = self.client.get(reverse('posts_list'))
+        response = self.client.get(reverse('posts_list'))    
         self.assertContains(response, self.post1.title)
         self.assertNotContains(response, self.post2.title)
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_create'), {
+            'title': 'Some Title',
+            'text': 'This is some text!',
+            'status': 'pub',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Some Title')
+        self.assertEqual(Post.objects.last().text, 'This is some text!')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_update', args=[self.post2.id]), {
+            'title': 'Some Title Update',
+            'text': 'This is some text for Update!',
+            'status': 'pub',
+            'author': self.post2.author.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Some Title Update')
+        self.assertEqual(Post.objects.last().text, 'This is some text for Update!')
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args=[self.post2.id]), )
+        self.assertEqual(response.status_code, 302)
