d5169dc152fa4661bcf08a99000066d5d25b065b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-13 20:22:10 +0330

    using class-basedView instead of functionalView

--- blogWebsiteProjectDjango-with-MySql/blog/views.py
+++ blogWebsiteProjectDjango-with-MySql/blog/views.py
@@ -1,47 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Post
-from .forms import NewPostForm
+from .forms import PostForm
 
-def post_list_view(request):
-    posts_list = Post.objects.filter(status='pub').order_by('datetime_modified')
-    return render(request, 'blog/posts_list.html', context={'posts_list': posts_list})
+class PostListView(generic.ListView):
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts_list'
 
-def post_detail_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', context={'post': post})
-
-def post_create_view(request):
-    if request.method == 'POST':
-        form = NewPostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('posts_list')
-
-    else:
-        form = NewPostForm()     
-
-    return render(request, 'blog/post_create.html', context={'form': form} )
-
-def post_update_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    form = NewPostForm(request.POST or None, instance=post)
-
-    if form.is_valid():
-        form.save()
-        return redirect('posts_list')
-
-    return render(request, 'blog/post_create.html', context={'form': form})
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('datetime_modified')
 
 
-def post_delete_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
-    if request.method == 'POST':
-        post.delete()
-        return redirect('posts_list')
 
-    return render(request, 'blog/post_delete.html', context={'post': post} )
+class PostCreateView(generic.CreateView):
+    template_name = 'blog/post_create.html'
+    form_class = PostForm
 
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    template_name = 'blog/post_create.html'
+    form_class = PostForm
+
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('posts_list')
