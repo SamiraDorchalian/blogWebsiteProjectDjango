77ab8aa041db5d2793349ab79220a5a5fdadbcb0
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-14 13:50:57 +0330

    add login & logout page

--- blogWebsiteProjectDjango-with-MySql/config/settings.py
+++ blogWebsiteProjectDjango-with-MySql/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
 
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+
+LOGIN_REDIRECT_URL = 'posts_list'
+LOGOUT_REDIRECT_URL = 'posts_list'
